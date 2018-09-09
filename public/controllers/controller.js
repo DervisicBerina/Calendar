184f0a3ce337616f2fdee5ccae04a7cfef59e3ef
Berina Dervisic <dervisicberina@hotmail.com>
2018-09-09 12:59:53 +0200

    mycalendar

---
+++ public/controllers/controller.js
@@ -0,0 +1,77 @@
+var app = angular.module('myApp', ['toastr', '720kb.datepicker']);
+
+app.controller('myCtrl', ['$scope', '$http', 'toastr', function ($scope, $http, toastr) {
+    console.log('tudek');
+    $scope.addButtonVisible = false;
+    $scope.hideAddButton = function () {
+        $scope.addButtonVisible = false;
+    }
+    $scope.showAddButton = function () {
+        $scope.addButtonVisible = true;
+    }
+
+    $scope.open = function () {
+        $scope.visible = false;
+        $scope.visible = $scope.visible = true;
+    }
+
+    $scope.close = function () {
+        $scope.visible = true;
+        $scope.visible = $scope.visible = false;
+    }
+
+    $scope.confirm = function (id) {
+        var id = id
+        $scope.visible2 = false;
+        $scope.visible2 = $scope.visible2 = true;
+        $scope.test = id
+    }
+    $scope.confirm2 = function () {
+        $scope.visible2 = true;
+        $scope.visible2 = $scope.visible2 = false;
+    }
+    var refresh = function () {
+        $http.get('/calendar').then(function (response) {
+            console.log("I got the data I requested");
+            $scope.eventList = response.data;
+        });
+    };
+
+    refresh()
+
+    $scope.submit = function () {
+        $http.post('/calendar', $scope.event).then(function (response) {
+            console.log(response);
+            refresh();
+            $scope.event.name = "";
+            $scope.event.date = "";
+            $scope.event.cost = "";
+            toastr.success('Event added successfully');
+        });
+    }
+    $scope.delete = function (id) {
+        console.log(id);
+        $http.delete('/calendar/' + id).then(function (response) {
+            refresh();
+            toastr.error('Event deleted successfully');
+        });
+    };
+    $scope.edit = function (id) {
+        console.log(id);
+        $http.get('/calendar/' + id).then(function (response) {
+            $scope.event = response.data;
+        });
+    };
+
+    $scope.update = function () {
+
+        $http.put('/calendar/' + $scope.event._id, $scope.event).then(function (response) {
+            refresh();
+            $scope.event.name = "";
+            $scope.event.date = "";
+            $scope.event.cost = "";
+            toastr.success('Item updated successfully');
+        })
+    };
+
+}]);
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express()
+
+const bodyparser = require("body-parser");
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.json()); // to support JSON-encoded bodies
+app.use(bodyparser.json());
+var urlencodedParser = bodyparser.urlencoded({
+    extended: false
+})
+var mongojs = require('mongojs')
+var db = mongojs('calendar', ['calendar'])
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/calendar', function (req, res) {
+    console.log('I received a GET request');
+    db.calendar.find(function (err, docs) {
+        console.log(docs);
+        res.json(docs);
+    });
+});
+
+app.post('/calendar', function (req, res) {
+    console.log(req.body);
+    db.calendar.insert(req.body, function (err, doc) {
+        res.json(doc);
+    });
+});
+app.delete('/calendar/:id', function (req, res) {
+    var id = req.params.id;
+    console.log(id);
+    db.calendar.remove({ _id: mongojs.ObjectId(id) }, function (err, doc) {
+        res.json(doc);
+    });
+});
+app.get('/calendar/:id', function (req, res) {
+    var id = req.params.id;
+    console.log(id);
+    db.calendar.findOne({ _id: mongojs.ObjectId(id) }, function (err, doc) {
+        res.json(doc);
+    });
+});
+
+app.put('/calendar/:id', function (req, res) {
+    var id = req.params.id;
+    console.log(req.body.date);
+    db.calendar.findAndModify({
+        query: { _id: mongojs.ObjectId(id) },
+        update: { $set: { date: req.body.date, name: req.body.name, cost: req.body.cost } },
+        new: true
+    }, function (err, doc) {
+        res.json(doc);
+    }
+    );
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
